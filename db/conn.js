2b183ba799b446b6f4619cbdab22936bd1a85949
Orion Teles de Mesquita <orionteles@gmail.com>
2024-03-20 19:11:39 -0300

    Mongo

--- db/conn.js
+++ db/conn.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 async function main(){
 
     try {
-        await mongoose.connect('mongodb+srv://orionmesquita:IpqGvqmeDIhvuEDt@cluster0.ae8ng2a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+        await mongoose.connect('')
         console.log('Banco OK');
     } catch (error) {
         console.log('Erro: ' + error);
